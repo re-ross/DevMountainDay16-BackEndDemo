0c0ce4097c6f1e79ac6d9b9dc89195fdf5011e0a
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-24 12:40:39 -0600

    added basic funcionality: get request, get request with param, get request using query

--- server/index.js
+++ server/index.js
@@ -19,10 +19,24 @@ const mockDb = [
 ];
 
 app.get("/api/inventory", (req, res) => {
-  console.log("hit");
-  //   res.sendStatus(200);
-  res.status(200).send(mockDb);
-  console.log("hit2");
+  const { item } = req.query;
+
+  if (item) {
+    const filteredItems = mockDb.filter((el) => {
+      return el.includes(item);
+    });
+    res.status(200).send(filteredItems);
+  } else {
+    console.log("hit");
+    //   res.sendStatus(200);
+    res.status(200).send(mockDb);
+  }
 });
+
+app.get("/api/inventory/:itemInd", (req, res) => {
+  res.status(200).send(mockDb[+req.params.itemInd]);
+});
+
+app.get("/api/inventory", (req, res) => {});
 
 app.listen(5050, () => console.log("Server running on port 5050"));
